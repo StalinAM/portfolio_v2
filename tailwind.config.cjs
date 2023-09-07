e810363b9ecbcb1189fb260ef0719862ed5cbfba
Stalin Acurio <sta_vla411@hotmail.com>
2023-09-06 21:32:51 -0500

    add custom display property

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      js: "#fbae17",
+    },
+    gridTemplateColumns: {
+      "box-xl": "repeat(auto-fill, minmax(300px, 1fr))",
+      "box-x": "repeat(auto-fill, minmax(250px, 1fr))",
+    },
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+};
